4806e7c947896ba4829b027946e0521b2977abd9
Jimmy Woolley <jamesmwoolley@gmail.com>
2013-12-30 15:35:55 -0700

    linted

--- download.js
+++ download.js
@@ -10,37 +10,38 @@ var totalFinishedDownloads = 0;
 var config = JSON.parse(fs.readFileSync('config.json'));
 
 var queue = async.queue(function (task, cb) {
-	var stream = fs.createWriteStream(task.filepath);
-	stream.on('close', function() {
-		totalFinishedDownloads += 1;
-		var name = path.basename(task.url);
-		var dest = path.dirname(task.filepath);
-		var now = moment().format(config.mfmt);
-		var msg = 'downloaded ' + name + ' to ' + dest + ' on ' + now;
-		console.log(msg);
-		cb();
-	});
-	stream.on('error', function (err) {
-		cb(err);
-	});
-	request(task.url).pipe(stream);
+  var stream = fs.createWriteStream(task.filepath);
+  stream.on('close', function () {
+    totalFinishedDownloads += 1;
+    var name = path.basename(task.url);
+    var dest = path.dirname(task.filepath);
+    var now = moment().format(config.mfmt);
+    var msg = 'downloaded ' + name + ' to ' + dest + ' on ' + now;
+    console.log(msg);
+    cb();
+  });
+  stream.on('error', function (err) {
+    cb(err);
+  });
+  request(task.url).pipe(stream);
 }, config.queueConcurrency);
 
 queue.drain = function () {
-	var tfd = totalFinishedDownloads.toString();
-	var trd = totalRequestedDownloads.toString();
-	var msg = '(' + tfd + '/' + trd + ') files downloaded successfully';
-	console.log(msg);
-	totalRequestedDownloads = 0;
-	totalFinishedDownloads = 0;
+  var tfd = totalFinishedDownloads.toString();
+  var trd = totalRequestedDownloads.toString();
+  var msg = '(' + tfd + '/' + trd + ') files downloaded successfully';
+  console.log(msg);
+  totalRequestedDownloads = 0;
+  totalFinishedDownloads = 0;
 };
 
-var get = function(url, filepath) {
-	totalRequestedDownloads += 1;
-	queue.push({"url": url, "filepath": filepath}, function (err) {
-		if (err)
-			throw err;
-	});
+var get = function (url, filepath) {
+  totalRequestedDownloads += 1;
+  queue.push({"url": url, "filepath": filepath}, function (err) {
+    if (err) {
+      throw err;
+    }
+  });
 };
 
 exports.get = get;
